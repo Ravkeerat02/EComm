683592f3e3148d691f65f2f5d4b2cc15c10d60e4
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2024-02-05 02:17:29 -0500

    setup

---
+++ store/store/store/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path , include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # backend link
+    path('api/v1',include('djoser.urls')),    
+    path('api/v1',include('djoser.urls.authtoken'))
+]
+
+
+
+
